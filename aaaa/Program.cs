5672196683e9520ac242ce393cdcb3d6574a82d6
Selahattin Kara <Selahattin Kara@DESKTOP-JNQH4OR>
2022-03-14 23:28:26 +0300

    Add project files.

---
+++ aaaa/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaaa
+{
+    public class Abc
+    {
+
+        public static void fizzBuzz()
+        {
+            for (int i = 1; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+
+                }
+                else if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Abc Abc = new Abc();
+            Abc.fizzBuzz();
+
+            Console.ReadKey();
+        }
+    }
+
+}
